be0b5a7b95fdeceee9c2f620d511cdefd21c7e39
Mike <34860474+MRobi1@users.noreply.github.com>
2025-08-12 13:31:31 -0300

    Create Plugin.cs

---
+++ Plugin.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using Emby.Plugin.TubeArchivistMetadata.Configuration;
+
+namespace Emby.Plugin.TubeArchivistMetadata
+{
+    /// <summary>
+    /// The main plugin.
+    /// </summary>
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Plugin"/> class.
+        /// </summary>
+        /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
+        /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        /// <inheritdoc />
+        public override string Name => "TubeArchivist Metadata";
+
+        /// <inheritdoc />
+        public override Guid Id => Guid.Parse("d4c0ee41-f5c8-4c84-9dc7-8e6c9e2b8a91");
+
+        /// <inheritdoc />
+        public override string Description => "Metadata provider for TubeArchivist";
+
+        /// <summary>
+        /// Gets the current plugin instance.
+        /// </summary>
+        public static Plugin? Instance { get; private set; }
+
+        /// <inheritdoc />
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = this.Name,
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
+                }
+            };
+        }
+    }
+}
