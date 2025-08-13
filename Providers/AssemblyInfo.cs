3c006530fe3cd9210859afb140ac8895f1fb3cce
Mike <34860474+MRobi1@users.noreply.github.com>
2025-08-13 09:39:49 -0300

    Create AssemblyInfo.cs

---
+++ Providers/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Emby.Plugin.TubeArchivistMetadata")]
+[assembly: AssemblyDescription("TubeArchivist metadata provider for Emby")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Emby.Plugin.TubeArchivistMetadata")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Plugin GUID - This should be generated once and never changed
+[assembly: System.Runtime.InteropServices.Guid("A8B5B444-8C8D-4F2A-9B1C-3D4E5F6A7B8C")]
